e247e4517b57a9af30bdecf559e9e8cb90e617bc
Rafael Anzini Ximenes <anzinirafael50@gmail.com>
2022-01-18 14:22:11 -0300

    update

---
+++ src/com/company/RemoteController.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class RemoteController implements Controller{
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public RemoteController() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean isTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+}
